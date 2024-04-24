28943dcd2c249e39cb36bde136dba3f17a07c066
arun-s3 <arunsudhakaran01@gmail.com>
2024-04-25 00:21:21 +0530

    2nd version of todo project

--- server/Model/todoModel.js
+++ server/Model/todoModel.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 const todoSchema = mongoose.Schema({
-    task: String,
-    done:{type:Boolean,
-        default:false}
+    title: String,
+    done:{
+        type:Boolean,
+        default:false
+    },
+    desc:String,
+    date:String
 });
 
 const todoModel = mongoose.model("Todo", todoSchema);
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/todo");
+mongoose.connect("mongodb://localhost:27017/todo-v2");
 mongoose.connection.on("connected", ()=>console.log("Connected to database..."))
 mongoose.connection.on("error", (error)=>console.log(error))
 mongoose.connection.on("disconnected", ()=>console.log("Disconnected from the database..."))
@@ -15,8 +15,8 @@ app.use(cors());
 const todoModel = require('./Model/todoModel');
 
 app.post('/add', (req,res)=>{
-    const task = req.body.task
-    todoModel.create({task:task})
+    const {title, desc, date} = req.body.task
+    todoModel.create({title:title, desc:desc, date:date})
              .then(result=>res.json(result))
              .catch(error=>console.log(error))
 })
@@ -28,11 +28,25 @@ app.get('/tasks', (req,res)=>{
 
 app.put('/done/:id', (req,res)=>{
     const {id} = req.params
-    todoModel.findByIdAndUpdate({_id:id},{done:true})
+    todoModel.findByIdAndUpdate({_id:id},{done:req.body.done})
              .then(result=> res.json(result))
              .catch(error=> res.json(error))
 })
-
+app.put('/update/:id',(req,res)=> {
+    const {id} = req.params
+    req.body.title? todoModel.findByIdAndUpdate({_id:id},{title:req.body.title})
+                             .then(result=> res.json(result))
+                             .catch(error=> res.json(error))
+                  : todoModel.findByIdAndUpdate({_id:id},{desc:req.body.desc})
+                             .then(result=> res.json(result))
+                             .catch(error=> res.json(error))
+})
+app.put('/done/:id', (req,res)=>{
+    const {id} = req.params
+    todoModel.FindByIdAndUpdate({_id:id},{done:req.body.done})
+             .then(result=> res.json(result))
+             .catch(error=> res.json(error))
+})
 app.delete('/delete/:id', (req,res)=>{
     const {id} = req.params
     todoModel.deleteOne({_id:id})
